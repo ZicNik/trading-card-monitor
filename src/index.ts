4079970ca40ea0fc4e167f5ec5a7aaf2fb56706b
Giorgio Conti <gio.conti90@gmail.com>
2026-03-02 18:16:36 +0100

    Removed the concept of 'UserActivity', now that context is given by the bot UI. Also, removed the now unneeded 'SearchInitiatedUseCase'.

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ import type { CardCatalog } from '@/search'
 import { User, type UserRepository } from '@/user'
 import { BotUI } from './bot-ui/bot-ui'
 import { InMemoryStateMachineStorage } from './bot-ui/state-machine-storage'
-import { GRAMMY_BOT } from './grammy'
 import { GrammyInputPort } from './grammy/input-port'
-import { GrammyOutputPort } from './grammy/output-port'
-import { SearchInitiatedPresenter, SearchInitiatedUseCase, type SearchInitiatedView, type SearchInitiatedViewModel } from './search/search-initiated'
 
 class TestUserRepository implements UserRepository {
   private readonly users = new Map<string, User>()
@@ -25,28 +22,10 @@ class TestUserRepository implements UserRepository {
   }
 }
 
-class ConsoleSearchInitiatedView implements SearchInitiatedView {
-  render(vm: SearchInitiatedViewModel): void {
-    console.log(`Search initiated for user ${vm.recipientId}`)
-  }
-}
-
-class BotSearchInitiatedView implements SearchInitiatedView {
-  render(vm: SearchInitiatedViewModel): void {
-    void GRAMMY_BOT.api.sendMessage(vm.recipientId, 'Search initiated')
-  }
-}
-
 // Compose dependencies
 const scryfallApis = new ScryfallApis({ timeoutMs: 7000, retries: 3 })
 const catalog: CardCatalog = new ScryfallCatalog(scryfallApis)
-const userRepo: UserRepository = new TestUserRepository()
-const consoleSearchInitiatedView: SearchInitiatedView = new ConsoleSearchInitiatedView()
-const consoleSearchInitiatedPresenter = new SearchInitiatedPresenter(consoleSearchInitiatedView)
-const consoleSearchInitiatedUseCase = new SearchInitiatedUseCase(userRepo, consoleSearchInitiatedPresenter)
-const botSearchInitiatedView: SearchInitiatedView = new BotSearchInitiatedView()
-const botSearchInitiatedPresenter = new SearchInitiatedPresenter(botSearchInitiatedView)
-const botSearchInitiatedUseCase = new SearchInitiatedUseCase(userRepo, botSearchInitiatedPresenter)
+const botUI = new BotUI(new GrammyInputPort(), new InMemoryStateMachineStorage())
 
 // Example usage of the CardCatalog
 
@@ -70,40 +49,6 @@ async function testCardCatalog() {
 //       .catch(console.error)
 //   })
 //   .catch(console.error)
-
-// Example usage of the SearchInitiatedUseCase
-
-function testSearchInitiatedUseCaseOnConsole() {
-  const userBefore = userRepo.findById('1')
-  console.log('Before:', userBefore)
-  consoleSearchInitiatedUseCase.execute({ userId: '1' })
-  const userAfter = userRepo.findById('1')
-  console.log('After:', userAfter)
-}
-
-// testSearchInitiatedUseCaseOnConsole()
-
-function setupSearchInitiatedUseCaseOnBot() {
-  // Controller triggering the use case
-  // GRAMMY_BOT.command('search', (ctx) => {
-  //   if (ctx.from?.id === undefined)
-  //     return
-  //   const userId = ctx.from.id.toString()
-  //   botSearchInitiatedUseCase.execute({ userId })
-  // })
-  const inputPort = new GrammyInputPort()
-  const outputPort = new GrammyOutputPort()
-  inputPort.onCommand('search', async (context) => {
-    await outputPort.sendMessage(context.chatId, `Received search command`)
-  })
-  inputPort.onMessage(async (context) => {
-    await outputPort.sendMessage(context.chatId, `Received message: ${context.text}`)
-  })
-}
-
-// setupSearchInitiatedUseCaseOnBot()
-
-const botUI = new BotUI(new GrammyInputPort(), new InMemoryStateMachineStorage())
 
 function testBotUI() {
   botUI.start()
--- src/user/user.ts
+++ src/user/user.ts
@@ -1,7 +1,6 @@
 export class User {
   constructor(
     public readonly id: string,
-    public currentActivity: UserActivity = { type: 'idle' },
   ) {}
 }
 
@@ -11,16 +10,3 @@ export interface UserRepository {
   findById(id: string): User | undefined
   save(user: User): void
 }
-
-// MARK: - Activity
-
-/** @see {@link UserActivityRegistry} */
-export type UserActivity = UserActivityRegistry[keyof UserActivityRegistry]
-
-/** Modules that add new activities are expected to extend this interface. In doing so, ensure that each activity has a `type` field with a unique string literal type. */
-export interface UserActivityRegistry {
-  idle: IdleActivity
-}
-
-/** A user not engaged in any activity. */
-export type IdleActivity = Readonly<{ type: 'idle' }>
