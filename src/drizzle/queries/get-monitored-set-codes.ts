d19c173bbeb9c69b821da9bdc163e1345df90a36
Giorgio Conti <gio.conti90@gmail.com>
2026-07-30 14:48:01 +0200

    Removed unneeded query

--- src/drizzle/queries/get-monitored-set-codes.ts
+++
@@ -1,8 +0,0 @@
-import type { GetMonitoredSetCodesQuery, GetMonitoredSetCodesResult } from '@/core'
-import { DRIZZLE_DB } from '../db'
-
-export class DbGetMonitoredSetCodesQuery implements GetMonitoredSetCodesQuery {
-  async execute(): Promise<GetMonitoredSetCodesResult> {
-
-  }
-}
