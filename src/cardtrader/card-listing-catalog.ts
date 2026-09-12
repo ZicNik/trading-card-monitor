2d03eca79e85340ac32adeddbc0e8ac8267001f8
Giorgio Conti <gio.conti90@gmail.com>
2026-09-12 19:19:19 +0200

    Made CardTraderListingCatalog more resilient by adding search key normalization

--- src/cardtrader/card-listing-catalog.ts
+++ src/cardtrader/card-listing-catalog.ts
@@ -3,6 +3,7 @@ import { eq } from 'drizzle-orm'
 import { CardListing, type CardListingCatalog } from '@/core'
 import { DRIZZLE_DB } from '@/drizzle/db'
 import { cardtraderBlueprintsTable } from '@/drizzle/schema'
+import { normalizeSearchKey } from '@/drizzle/utils'
 
 import type { CardTraderApis } from './apis'
 import { cardtraderProductToCardListing } from './mappers'
@@ -13,8 +14,11 @@ export class CardTraderListingCatalog implements CardListingCatalog {
   async findByCardName(name: string): Promise<CardListing<'cardtrader'>[]> {
     const blueprints = await DRIZZLE_DB.select()
       .from(cardtraderBlueprintsTable)
-      .where(eq(cardtraderBlueprintsTable.name, name))
-    return (await Promise.all(blueprints.map(async b => [b.id, await this.apis.marketplaceProducts({ blueprint_id: b.id })] as const)))
-      .flatMap(([id, products]) => products?.[id]?.map(p => cardtraderProductToCardListing(name, p)) ?? [])
+      .where(eq(cardtraderBlueprintsTable.normalized_name, normalizeSearchKey(name)))
+    const cardName = blueprints[0]?.name
+    return cardName !== undefined
+      ? (await Promise.all(blueprints.map(async b => [b.id, await this.apis.marketplaceProducts({ blueprint_id: b.id })] as const)))
+          .flatMap(([id, products]) => products?.[id]?.map(p => cardtraderProductToCardListing(cardName, p)) ?? [])
+      : []
   }
 }
