82e6731092f5de0224c915dee95b9818a53a6928
Giorgio Conti <gio.conti90@gmail.com>
2026-09-23 17:52:55 +0200

    - Refined ActorView contract - Adjusted Message and EditedMessage views - Added CombinedView

--- src/bot-ui/views/views.ts
+++ src/bot-ui/views/views.ts
@@ -1,11 +1,11 @@
-import { fromPromise, type AnyActorLogic, type PromiseActorLogic } from 'xstate'
+import { createActor, fromPromise, toPromise, type PromiseActorLogic } from 'xstate'
 
 import type { BotEnvironment } from '../bot-environment'
 import type { MessageInfo, MessageOptions } from '../output'
 
 /** A view that is able to turn into actor logic for a state machine. */
-export interface ActorView<A extends AnyActorLogic> {
-  toActor(): A
+export interface ActorView<Output, Input = unknown> {
+  toActor(): PromiseActorLogic<Output, Input>
 }
 
 // MARK: - Message
@@ -15,15 +15,15 @@ export type MessageViewModel = Readonly<{
   options?: MessageOptions
 }>
 
-type MessageViewActorInput<Input> = { chatId: string } & Input
+export type MessageActorInput<Input = unknown> = { chatId: string } & Input
 
 /** View representing a new chat message. */
-export class Message<Input> implements ActorView<PromiseActorLogic<MessageInfo, MessageViewActorInput<Input>>> {
+export class Message<Input = unknown> implements ActorView<MessageInfo, MessageActorInput<Input>> {
   private constructor(
     private readonly viewmodelBuilder: ({ input, env }: { input: Input, env: BotEnvironment }) => MessageViewModel,
   ) {}
 
-  static withText(text: string, options?: MessageOptions): Message<unknown> {
+  static withText(text: string, options?: MessageOptions): Message {
     return new Message(() => ({ text, ...(options === undefined ? {} : { options }) }))
   }
 
@@ -35,7 +35,7 @@ export class Message<Input> implements ActorView<PromiseActorLogic<MessageInfo, 
     return new Message(viewmodelBuilder)
   }
 
-  toActor(): PromiseActorLogic<MessageInfo, MessageViewActorInput<Input>> {
+  toActor(): PromiseActorLogic<MessageInfo, MessageActorInput<Input>> {
     return fromPromise(({ input, system }) => {
       const vm = this.viewmodelBuilder({ input, env: system.env })
       return system.env.outputPort.sendMessage(input.chatId, vm.text, vm.options)
@@ -46,15 +46,15 @@ export class Message<Input> implements ActorView<PromiseActorLogic<MessageInfo, 
 // MARK: - EditedMessage
 
 export type EditedMessageViewModel = MessageViewModel
-type EditedMessageViewActorInput<Input> = { messageId: string } & MessageViewActorInput<Input>
+export type EditedMessageActorInput<Input = unknown> = { messageId: string } & MessageActorInput<Input>
 
 /** View representing an edited chat message. */
-export class EditedMessage<Input> implements ActorView<PromiseActorLogic<void, EditedMessageViewActorInput<Input>>> {
+export class EditedMessage<Input = unknown> implements ActorView<void, EditedMessageActorInput<Input>> {
   private constructor(
     private readonly viewmodelBuilder: ({ input, env }: { input: Input, env: BotEnvironment }) => EditedMessageViewModel,
   ) {}
 
-  static withText(text: string, options?: MessageOptions): EditedMessage<unknown> {
+  static withText(text: string, options?: MessageOptions): EditedMessage {
     return new EditedMessage(() => ({ text, ...(options === undefined ? {} : { options }) }))
   }
 
@@ -66,10 +66,44 @@ export class EditedMessage<Input> implements ActorView<PromiseActorLogic<void, E
     return new EditedMessage(viewmodelBuilder)
   }
 
-  toActor(): PromiseActorLogic<void, EditedMessageViewActorInput<Input>> {
+  toActor(): PromiseActorLogic<void, EditedMessageActorInput<Input>> {
     return fromPromise(({ input, system }) => {
       const vm = this.viewmodelBuilder({ input, env: system.env })
       return system.env.outputPort.editMessage(input.chatId, input.messageId, vm.text, vm.options)
+    })
+  }
+}
+
+// MARK: - CombinedView
+
+type ViewsBuilder<Output, Input> = ({ input, env }: { input: Input, env: BotEnvironment }) => ActorView<Output, Input>[]
+
+/** Render multiple views sequentially. */
+export class CombinedView<Output, Input> implements ActorView<Output[], Input> {
+  private constructor(
+    private readonly builder: ViewsBuilder<Output, Input>,
+  ) {}
+
+  static from<Output, Input>(...views: ActorView<Output, Input>[]): CombinedView<Output, Input> {
+    return new CombinedView(() => views)
+  }
+
+  static fromBuilder<Output, Input>(builder: ViewsBuilder<Output, Input>): CombinedView<Output, Input> {
+    return new CombinedView(builder)
+  }
+
+  toActor(): PromiseActorLogic<Output[], Input> {
+    return fromPromise(async ({ input, self }) => {
+      const output: Output[] = []
+      for (const view of this.builder({ input, env: self.system.env })) {
+        const actor = createActor(view.toActor(), {
+          input,
+          parent: self,
+        })
+        actor.start()
+        output.push(await toPromise(actor))
+      }
+      return output
     })
   }
 }
