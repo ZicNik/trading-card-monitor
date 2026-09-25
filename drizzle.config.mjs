70d728c6bdaf91212a9a1ee49c74c44afa4c2a48
Giorgio Conti <gio.conti90@gmail.com>
2026-09-25 16:52:26 +0200

    Widened db and application configs

--- drizzle.config.mjs
+++ drizzle.config.mjs
@@ -2,10 +2,11 @@ import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
-  dialect: 'sqlite',
-  out: './drizzle',
+  dialect: process.env.DB_DIALECT,
+  out: './db-migration',
   schema: './src/drizzle/schema.ts',
   dbCredentials: {
     url: process.env.DB_URL,
+    authToken: process.env.DB_TOKEN,
   },
 })
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,5 +2,8 @@ import { APP_CONFIG } from '@/config'
 import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
 
-const client = createClient({ url: APP_CONFIG.dbUrl })
+const client = createClient({
+  url: APP_CONFIG.dbUrl,
+  ...(APP_CONFIG.dbToken !== undefined ? { authToken: APP_CONFIG.dbToken } : {}),
+})
 export const DRIZZLE_DB = drizzle({ client })
